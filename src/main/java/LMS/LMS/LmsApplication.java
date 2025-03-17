487e9eb3bc24fca543bbd852d49b62968621bddc
Mohammed desktop <mohammed.alsalhi@outlook.com>
2025-03-17 08:35:35 +0400

    This is LMG project

---
+++ src/main/java/LMS/LMS/LmsApplication.java
@@ -0,0 +1,15 @@
+package LMS.LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsApplication {
+
+	// This is LMG project
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsApplication.class, args);
+	}
+
+}
